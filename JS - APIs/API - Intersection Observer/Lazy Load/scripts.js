e86856412fffdfcc193ea6963caccb678daae190
Jose <jsissame3484@universidadean.edu.co>
2022-10-09 18:39:38 -0500

    lazy load

--- JS - APIs/API - Intersection Observer/Lazy Load/scripts.js
+++ JS - APIs/API - Intersection Observer/Lazy Load/scripts.js
@@ -1,1 +1,34 @@
 const images = document.getElementById('images')
+
+const getImages = () => {
+    axios('https://picsum.photos/v2/list?page=3&limit=5')
+        .then(res => {
+            const fragment = document.createDocumentFragment()
+            res.data.forEach(element => {
+                const newImage = document.createElement('IMG')
+                newImage.src = element.download_url
+                fragment.appendChild(newImage)
+            })
+            images.appendChild(fragment)
+            setObserver()
+        })
+}
+
+const callback = (entries) => {
+    entries.forEach(elem => {
+        if(elem.isIntersecting) {
+            getImages();
+        }
+    })
+}
+
+const options = {
+    threshold : 0.5
+}
+
+const setObserver = () => {
+    const observer = new IntersectionObserver(callback, options)
+    observer.observe(images.lastElementChild)
+}
+
+getImages()
