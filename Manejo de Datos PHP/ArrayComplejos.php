1d48646eab19dbab0105dda14d1c6e1c9745a529
Jose <jsissame3484@universidadean.edu.co>
2022-11-14 21:45:19 -0500

    Funciones array php

---
+++ Manejo de Datos PHP/ArrayComplejos.php
@@ -0,0 +1,43 @@
+<?php 
+
+// $courses = [
+//     "frontend" => "javascript",
+//     "backend" => "php",
+//     "framework" => "angular"
+// ];
+
+// function greet(Closure $c, $courses) {
+//     array_walk($courses, $c);
+// }
+
+// $c = function($course) {
+//     echo "$course <br>"
+// };
+
+// greet($c, $courses)
+
+/*
+
+sort: Ordena un array.
+
+rsort: Ordena un array en orden inverso.
+
+ksort: Ordena un array por clave.
+
+krsort: Ordena un array por clave en orden inverso.
+
+array_slice: Extrae una parte de un array.
+
+array_chunk: Divide un array en fragmentos.
+
+array_shift: Quita un elemento del principio del array.
+
+array_pop: Extrae el último elemento del final del array.
+
+array_unshift: Añadir al inicio de un array uno a más elementos.
+
+array_push: Inserta uno o más elementos al final de un array.
+
+array_flip: Intercambia todas las claves de un array con sus valores asociados.
+
+*/
