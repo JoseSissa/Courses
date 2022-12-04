75c3959a2eaf55dbf7b933f078469741e1d249de
Jose Sissa <jsissame3484@universidadean.edu.co>
2022-12-04 11:38:31 -0500

    try catch

---
+++ AsincronismoJS/src/TryCatch/trycatch.js
@@ -0,0 +1,23 @@
+const API = 'https://api.escuelajs.co/api/v1'
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi)
+    const data = await response.json()
+    return data
+}
+
+const anotherFunction = async (urlApi) => {
+    try {
+        const products = await fetchData(`${urlApi}/products`)
+        const product = await fetchData(`${urlApi}/products/${products[0].id}`)
+        const category = await fetchData(`${urlApi}/categories/${product.category.id}`)
+
+        console.log(products);
+        console.log(product.title);
+        console.log(category.name);
+    }catch (err) {
+        console.error(err);
+    }
+}
+
+anotherFunction(API)
